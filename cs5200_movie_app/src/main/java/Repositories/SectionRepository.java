13afbc1b7b6f7f07d08cdc54da6e615fcebf8dfa
bowenwu0826 <68851709+bowenwu0826@users.noreply.github.com>
2022-04-13 18:21:23 -0700

    bowen

--- cs5200_movie_app/src/main/java/Repositories/SectionRepository.java
+++ cs5200_movie_app/src/main/java/Repositories/SectionRepository.java
@@ -1,8 +1,17 @@
 package Repositories;
 
+import java.util.List;
 import models.Section;
+import models.Ticket;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface SectionRepository extends CrudRepository<Section, Integer>{
+
+  @Query(value = "SELECT * FROM sections WHERE theater_id = :id", nativeQuery = true)
+  public List<Section> getSectionsByTheater_id(@Param("id") Integer id);
+
 
 }
