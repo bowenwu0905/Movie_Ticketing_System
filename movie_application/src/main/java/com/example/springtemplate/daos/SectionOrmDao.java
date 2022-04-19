e18f9182b82cfd441a8d0b0b6f17890fe560d5f4
bowenwu0826 <68851709+bowenwu0826@users.noreply.github.com>
2022-04-19 13:23:36 -0700

    update_bw

--- movie_application/src/main/java/com/example/springtemplate/daos/SectionOrmDao.java
+++ movie_application/src/main/java/com/example/springtemplate/daos/SectionOrmDao.java
@@ -5,18 +5,56 @@ import com.example.springtemplate.repositories.SectionRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class SectionOrmDao {
+
   @Autowired
   SectionRepository sectionRepository;
 
-  @GetMapping("/girlspower/sections")
-  public List<Section> findAllSections(){
-    return sectionRepository.findAllSections();
+  @PostMapping("/girlspower/sections")
+  public Section createSection(@RequestBody Section section) {
+    return sectionRepository.save(section);
   }
 
+  @GetMapping("/girlspower/sections")
+  public List<Section> findAllSections() {
+    return (List<Section>) sectionRepository.findAll();
+  }
+
+  @GetMapping("/girlspower/sections/{sectionId}")
+  public Section findSectionBySectionId(@PathVariable("sectionId") Integer id) {
+    return sectionRepository.findById(id).get();
+  }
+
+  @PutMapping("/girlspower/sections/{sectionId}")
+  public Section updateSection(
+      @PathVariable("sectionId") Integer id,
+      @RequestBody() Section newSection) {
+    Section section = this.findSectionBySectionId(id);
+    section.setMovie_id(newSection.getMovie_id());
+    section.setTheater_id(newSection.getTheater_id());
+    section.setShowtime(newSection.getShowtime());
+    section.setRoom_number(newSection.getRoom_number());
+    return sectionRepository.save(section);
+  }
+
+/*  @DeleteMapping("/girlspower/sections/{sectionId}")
+  public void deleteSection(@PathVariable("sectionId") Integer id) {
+    List<Ticket> tickets = ticketRepository.findTicketsBySectionId(id);
+    for (Ticket ticket : tickets) {
+      ticketRepository.deleteById(ticket.getTicketID());
+    }
+    sectionRepository.deleteById(id);
+
+
+  }*/
 }
--- movie_application/src/main/java/com/example/springtemplate/repositories/SectionRepository.java
+++ movie_application/src/main/java/com/example/springtemplate/repositories/SectionRepository.java
@@ -7,7 +7,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface SectionRepository extends CrudRepository<Section, Integer> {
 
-  @Query(value = "SELECT * FROM sections",
-      nativeQuery = true)
-  public List<Section> findAllSections();
 }
