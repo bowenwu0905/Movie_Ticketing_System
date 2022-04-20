d6319dd219bedf317757754ba8043d1851425cde
bowenwu0826 <68851709+bowenwu0826@users.noreply.github.com>
2022-04-19 18:15:06 -0700

    new_ver

--- 5200_movie_application/src/main/java/com/example/_movie_application/Repositories/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example._movie_application.Repositories;
-
-import com.example._movie_application.models.Movie;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovieRepository extends CrudRepository<Movie, Integer>{
-
-}
--- 5200_movie_application/src/main/java/com/example/_movie_application/Repositories/TheaterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example._movie_application.Repositories;
-
-import com.example._movie_application.models.Theater;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TheaterRepository  extends CrudRepository<Theater, Integer> {
-
-
-}
